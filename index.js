d69ad2a31a1104957813796fd20cb53cb03b46d5
curran <curran.kelleher@gmail.com>
2015-09-28 15:35:51 -0700

    Port implementation from magic bar chart example

---
+++ index.js
@@ -0,0 +1,27 @@
+// chiasm-data-reduction
+// v__VERSION__
+// https://github.com/chiasm-project/chiasm-data-reduction
+
+var DataReduction = require("data-reduction");
+function ChiasmDataReduction (){
+
+  var my = new ChiasmComponent({
+    filter: Model.None,
+    aggregate: Model.None
+  });
+
+  my.when(["filter", "aggregate", "datasetIn"], function (filter, aggregate, datasetIn) {
+    var options = {};
+
+    if(filter !== Model.None){
+      options.filter = filter;
+    }
+    if(aggregate !== Model.None){
+      options.aggregate = aggregate;
+    }
+
+    my.datasetOut = DataReduction(datasetIn.data, options);
+  });
+
+  return my;
+}
